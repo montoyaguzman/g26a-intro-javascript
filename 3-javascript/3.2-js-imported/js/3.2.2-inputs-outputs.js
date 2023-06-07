94c6862bed92dcd8f8aaf09822aae40f9966a0cc
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-06-06 19:38:40 -0600

    feat: inputs y outputs en js y de typeof

---
+++ 3-javascript/3.2-js-imported/js/3.2.2-inputs-outputs.js
@@ -0,0 +1,6 @@
+var name = prompt('dime tu nombre?');
+
+console.log(name);
+alert(name);
+var tagHtml = '<h1>' + name + '</h1>';
+document.write(tagHtml);
---
+++ 3-javascript/3.2-js-imported/js/3.2.3-typeof.js
@@ -0,0 +1,31 @@
+var name = 'jose';
+var genre = 'M';
+var age = 30;
+var price = 30.99;
+var hasCredential = true;
+var sensei = {
+    name: 'daniel',
+    height: 90, 
+};
+var names = ['jose', 'moises', 'elena', 'alonso'];
+
+console.log(typeof name);
+console.log(name);
+
+console.log(typeof genre);
+console.log(genre);
+
+console.log(typeof age);
+console.log(age);
+
+console.log(typeof price);
+console.log(price);
+
+console.log(typeof hasCredential);
+console.log(hasCredential);
+
+console.log(typeof sensei);
+console.log(sensei);
+
+console.log(typeof names);
+console.log(names);
