d22f7b3b75d42bc4acb052f7e9b71b6046485d8b
dannielgloria <danniel.gloria@gmail.com>
2023-06-13 19:54:42 -0600

    feat: ejercicios del DOM

---
+++ 5-manipulacion-dom/js/5.1-cambio-de-text.js
@@ -0,0 +1,8 @@
+/**
+ * Cambiar el texto de un elemento: Crea un párrafo en HTML y, utilizando JavaScript,
+ * cambia su contenido por otro texto.
+ */
+
+var parrafo = document.getElementById("mi-parrafo")
+
+parrafo.textContent = "Nuevo texto en JS"
---
+++ 5-manipulacion-dom/js/5.2-cambio-fondo.js
@@ -0,0 +1,5 @@
+
+function cambiaFondo() {
+    var div = document.getElementById('miDiv');
+    div.style.backgroundColor = '#f9e64f';
+}
