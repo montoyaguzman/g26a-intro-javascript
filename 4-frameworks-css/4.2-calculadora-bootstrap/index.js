db2e87b17d380e781363e1e5217ed4fa7c613e41
montoyaguzman7 <montoyaguzman7@gmail.com>
2023-06-15 21:26:17 -0600

    feat: ejemplo de calculadora dom/bootstrap

--- 4-frameworks-css/4.2-calculadora-bootstrap/index.js
+++ 4-frameworks-css/4.2-calculadora-bootstrap/index.js
@@ -1,13 +1,41 @@
-const buttonOperationElement = document.querySelector('#operation-button');
+// const buttonOperationElement = document.querySelector('#operation-button');
+const showResButton = document.querySelector('#show-res-button');
+const selectOperationElement = document.querySelector('#operation-select');
+var symbolo = '';
 
-// html: onclick=""
-buttonOperationElement.addEventListener('click', () => {
+selectOperationElement.addEventListener('change', (event) => {
+    const selectElement = event.target;
+    symbolo = selectElement.options[selectElement.selectedIndex].text;
+})
+
+// // html: onclick=""
+showResButton.addEventListener('click', () => {
     const num1 = document.querySelector('#num1');
     const num2 = document.querySelector('#num2');
     // console.log(num1.value, num2.value);
     // console.log(num1.value, typeof num1.value);
-    const result = parseInt(num1.value) + parseInt(num2.value);
+    let result = 0;
+
+    if (symbolo === '+') {
+        result = parseInt(num1.value) + parseInt(num2.value);
+    } else  if (symbolo === '-') {
+        result = parseInt(num1.value) - parseInt(num2.value);
+    } else  if (symbolo === '*') {
+        result = parseInt(num1.value) * parseInt(num2.value);
+    } else  if (symbolo === '/') {
+        result = parseInt(num1.value) / parseInt(num2.value);
+    }
+    
     console.log('result', result)
     const divResult = document.querySelector('#result');
     divResult.innerHTML = result;
 })
+
+// buttonOperationElement.addEventListener('click', () => {
+//     const num1 = document.getElementById('num1');
+//     const num2 = document.getElementById('num2');
+//     const result = parseInt(num1.value) + parseInt(num2.value);
+//     console.log('result getElement', result)
+//     const divResult = document.getElementById('result');
+//     divResult.innerHTML = result;
+// })
